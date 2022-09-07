d8c6c2f4f6b008e4a90fa58e9ddbf93feb9cd7fe
SalahEddine Hanaoui <110311364+SuperGhostSama@users.noreply.github.com>
2022-09-07 16:29:58 +0100

    ### Challenge 1 : Affichage Informations

---
+++ Challenge 1  Affichage Informations/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char nom[10], prenom[10], age[2], sex[5], numeroTel[10] ;
+    scanf("Insert your name : %s", &nom );
+    printf("%s", &nom);
+    
+    
+return 0;
+
+}
