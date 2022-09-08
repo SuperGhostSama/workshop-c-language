a2acfbd8473b93f337ba9705acb9118d9975c4e7
SalahEddine Hanaoui <110311364+SuperGhostSama@users.noreply.github.com>
2022-09-08 23:07:53 +0100

    Add files via upload

---
+++ Challenge 13  Affichage de la valeur octale et hexadécimale équivalente/challenge 13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int var;
+    printf("entrer une valeur en Decimal : ");
+    scanf("%d",&var);
+    printf("en Hexadicimal est %x \n",var);
+    printf("en Octal est %o",var);
+    return 0;
+}
