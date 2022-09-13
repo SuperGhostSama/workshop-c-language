2e44e74f26532e4b5dc15687854b73b4079eeb42
Salah <salaheddinehanaoui@gmail.com>
2022-09-13 09:29:06 +0100

    Correction erreur

--- Week 1 Projects/Second Doc/Challenge 11/Challenge_11.c
+++ Week 1 Projects/Second Doc/Challenge 11/Challenge_11.c
@@ -8,41 +8,39 @@ int main ()
    
    switch(r) {
    	    case 0 :
-		{
+		
    	 	printf("Lundi ",r);
 	    break;
-		}
+	
 	    case 1 :
-		{
+		
    	 	printf("Mardi ",r);
 	    break;
-		}	
+			
 		 case 2 :
-		{
+		
    	 	printf("Mercredi ",r);
 	    break;
-		}
+		
 		 case 3 :
-		{
+		
    	 	printf("Jeudi ",r);
 	    break;
-		}
+		
 		 case 4 :
-		{
+		
    	 	printf("Vendredi ",r);
 	    break;
-		}
+		
 		 case 5 :
-		{
+		
    	 	printf("Samedi ",r);
 	    break;
-		}
+		
 		 case 6 :
-		{
+		
    	 	printf("Dimanche ",r);
 	    break;
-		}
- 	 	
-   }
+	}
 return 0;	
 }
